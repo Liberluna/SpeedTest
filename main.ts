143090798552a20cbb92ae9b51debdf10077099e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-10 14:02:20 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/middleware.ts'
 
 const app = new Hono()
 
@@ -6,8 +7,10 @@ const speedHeaderHead = 'GET　/testyourspeed.deno.dev/speed HTTP/1.1\n'
 app.get('/speed', c => {
   
   const header = speedHeaderHead + [...c.req.headers.entries()].map(h => `${h[0]}:${h[1]}`).join('\n') + "\n"
-
+  
   return c.text((new Blob([header], { type: "text/plain" })).size)
 })
-
+app.get('/*', serveStatic({
+  root: '/public'
+}))
 Deno.serve(app.fetch)
