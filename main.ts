f611624694364258b2d4543c3b1aa8a2b313d96a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-10 13:48:44 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,11 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+const speedHeaderHead = 'GET　/testyourspeed.deno.dev/speed HTTP/1.1\n'
 app.get('/speed', c => {
   
+  const header = [...c.req.headers.entries()].map(h => `${h[0]}:${h[1]}`).join('\n')
+  return c.text(speedHeaderHead + header + "\n")
 })
 
 Deno.serve(app.fetch)
