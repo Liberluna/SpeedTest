8358ed4f1f92e826cdea2f9a45197f9486042d64
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-10 13:54:31 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,9 @@ const app = new Hono()
 const speedHeaderHead = 'GET　/testyourspeed.deno.dev/speed HTTP/1.1\n'
 app.get('/speed', c => {
   
-  const header = [...c.req.headers.entries()].map(h => `${h[0]}:${h[1]}`).join('\n')
-  return c.text(speedHeaderHead + header + "\n")
+  const header = speedHeaderHead + [...c.req.headers.entries()].map(h => `${h[0]}:${h[1]}`).join('\n') + "\n"
+
+  return c.text((new Blob([header], { type: "text/plain" })).size)
 })
 
 Deno.serve(app.fetch)
